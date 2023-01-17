027b90e9b2f89e4e59fff3a6b119c596e75dad13
OpenHSL <119412883+OpenHSL@users.noreply.github.com>
2023-01-17 14:47:49 +0400

    Create Mask.py

---
+++ Mask.py
@@ -0,0 +1,21 @@
+from numpy import array
+
+class Mask:
+    data: array
+    labels: dict
+    metadata: dict
+
+    def __init__(self):
+        pass
+
+    def load_from_png(self):
+        pass
+
+    def load_from_bmp(self):
+        pass
+
+    def load_from_mat(self):
+        pass
+
+    def load_from_TEMPLATE(self):
+        pass
