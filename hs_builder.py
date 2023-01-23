2e76358059d50359472d8a93fa2a8cd57ebfefba
f-neumann77 <f.neumann77@yandex.ru>
2023-01-23 11:45:08 +0400

    updated templates of load_from_rail_dev and load_from_rot_dev methods in hs_builder.py added new empty methods for frame preparations

--- hs_builder.py
+++ hs_builder.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from hsi import HSImage
-from hs_raw_pb_data import RawImagesData
+from hs_raw_pb_data import RawImagesData, RawVideoData
 
 
 class HSBuilder:
@@ -24,10 +24,30 @@ class HSBuilder:
 
     """
 
-    def __init__(self, hs_raw_data, metadata):
+    def __init__(self, path_to_data, metadata):
         self.hsi = None
-        self.hs_raw_data = hs_raw_data
+        self.path_to_data = path_to_data
         self.metadata = metadata
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def cut_target_area(self, frame: np.ndarray) -> np.ndarray:
+        pass
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def illumination_normalization(self, frame: np.ndarray) -> np.ndarray:
+        pass
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def trapezoid_normalization(self, frame: np.ndarray) -> np.ndarray:
+        pass
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def barrel_normalization(self, frame: np.ndarray) -> np.ndarray:
+        pass
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def rotary_normalization(self, frame: np.ndarray) -> np.ndarray:
+        pass
     # ------------------------------------------------------------------------------------------------------------------
 
     @staticmethod
@@ -47,22 +67,60 @@ class HSBuilder:
 
     def load_from_rail_dev(self, rail_iterator):
         """
-        Creates HSI from rail-device
-
+            Creates HSI from rail-device
+            Steps:
+                1) Takes target area from raw-frame
+                2) Correction barrel distortion
+                3) Correction trapezoid distortion
+                4) Correction illuminate heterogeneity
+                5) Future corrections
+                6) Added to Y-Z layers set
         """
         hsi_tmp = []
         for frame in rail_iterator:
-            tmp_layer = HSBuilder._some_preparation_on_frame(frame=frame)
-            hsi_tmp.append(tmp_layer)
+            tmp_layer = self.cut_target_area(frame=frame)
+            tmp_layer = self.barrel_normalization(frame=tmp_layer)
+            tmp_layer = self.trapezoid_normalization(frame=tmp_layer)
+            tmp_layer = self.illumination_normalization(frame=tmp_layer)
+            tmp_layer = HSBuilder._some_preparation_on_frame(frame=tmp_layer)
+            hsi_tmp.append(np.array(tmp_layer))
+
+        # this transpose is needed for replace axis for z-x-y to x-y-z
         self.hsi = np.array(hsi_tmp).transpose((1, 2, 0))
     # ------------------------------------------------------------------------------------------------------------------
 
-    def load_from_uav_dev(self):
+    def load_from_uav_dev(self, uav_iterator):
+        """
+            Creates HSI from uav-device
+            #TODO REPLACE to here spectru CODE!
+        """
         pass
     # ------------------------------------------------------------------------------------------------------------------
 
-    def load_from_rot_dev(self):
-        pass
+    def load_from_rot_dev(self, rotary_iterator):
+        """
+            Creates HSI from rotary-device
+            Steps:
+                1) Takes target area from raw-frame
+                2) Correction barrel distortion
+                3) Correction trapezoid distortion
+                4) Correction illuminate heterogeneity
+                5) Correction rotary distortion
+                6) Future corrections
+                7) Added to Y-Z layers set
+        """
+        hsi_tmp = []
+        for frame in rotary_iterator:
+            tmp_layer = self.cut_target_area(frame=frame)
+            tmp_layer = self.barrel_normalization(frame=tmp_layer)
+            tmp_layer = self.trapezoid_normalization(frame=tmp_layer)
+            tmp_layer = self.illumination_normalization(frame=tmp_layer)
+            tmp_layer = self.rotary_normalization(frame=tmp_layer)
+            tmp_layer = HSBuilder._some_preparation_on_frame(frame=tmp_layer)
+            hsi_tmp.append(np.array(tmp_layer))
+
+        # this transpose is needed for replace axis for z-x-y to x-y-z
+        self.hsi = np.array(hsi_tmp).transpose((1, 2, 0))
     # ------------------------------------------------------------------------------------------------------------------
 
     def get_hsi(self) -> HSImage:
