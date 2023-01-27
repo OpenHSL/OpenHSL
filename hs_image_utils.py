b4ad6eb30e12839a7da5ce05b7ed0128af601d95
Max Petrov <underburned@gmail.com>
2023-01-27 14:58:26 +0400

    add BaseIntEnum class

--- hs_image_utils.py
+++ hs_image_utils.py
@@ -1,3 +1,13 @@
-"""
-Something will be in the future...
-"""
+import enum
+
+
+class BaseIntEnum(enum.IntEnum):
+    """
+    Base class for int enumeration
+    """
+    def describe(self):
+        return self.name, self.value
+
+    @classmethod
+    def enum_names(cls):
+        return [v.name for v in list(cls)]
