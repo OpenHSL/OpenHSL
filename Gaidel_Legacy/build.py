aa2507135fa4949054be56ee59fd9a3edc182b79
Liptee <muzzone777@gmail.com>
2023-02-09 21:07:10 +0400

    Gaidel_Legacy comments

--- Gaidel_Legacy/build.py
+++ Gaidel_Legacy/build.py
@@ -14,69 +14,98 @@ import Gaidel_Legacy.slices as slices
 
 SPECTRUM_BOX_COLOR = (255, 0, 0)
 
-def build_by_gps_log(spectrum, gps_filename):
+def build_by_gps_log(spectrum: np.ndarray, gps_filename: str):
     n, m, _ = spectrum.shape
     gps = pd.read_csv(gps_filename, sep=settings.CSV_DELIMITER)
     gps = gps.loc[gps[settings.HEADER_CAM_ID] == settings.GPS_HYPERCAM_FRAME].head(m)
-    bands = interp(
-        spectrum,
-        latitude=gps[settings.HEADER_X].tolist(),
-        longitude=gps[settings.HEADER_Y].tolist(),
-        rel_alt=gps[settings.HEADER_REL_ALT].tolist(),
-        angle=gps[settings.HEADER_ANGLE].tolist(),
-    )
+
+    bands = interp(spectrum,
+                   latitude=gps[settings.HEADER_X].tolist(),
+                   longitude=gps[settings.HEADER_Y].tolist(),
+                   rel_alt=gps[settings.HEADER_REL_ALT].tolist(),
+                   angle=gps[settings.HEADER_ANGLE].tolist(),)
+    
     for i in range(n):
         band = bands[i, :, :]
         settings.BLUR_AUTO = True
         if settings.BLUR_AUTO:
             band = blur_image(band)
         bands[i, :, :] = band
+
     return np.array(bands)
 
-def save_slices(path, return_cube=False):
+def save_slices(path: str) -> np.ndarray:
+    """
+        Args:
+            path: path to video file
+        Returns:
+            cube: ndarray with cube from video
+            
+    """
     capture = cv2.VideoCapture(path)
-    width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    deep = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
-    ans = np.zeros(shape=(settings.config.spectral_bands_number, deep, width), dtype=np.uint8)
-    index = 0
+    width: int = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)) # width of video
+    height: int = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)) # height of video
+    deep: int = int(capture.get(cv2.CAP_PROP_FRAME_COUNT)) # count of frames in video
+
+    # create cube with shape (40, deep, width)
+    cube: np.ndarray = np.zeros(shape=(settings.config.spectral_bands_number, deep, width), dtype=np.uint8)
+    index = 0 # index in this code also means frame counter
     while capture.isOpened():
         result, frame = capture.read()
         if not result:
             break
         else:
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY, frame)
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-        s, _ = slices.get_principal_slices(frame[int(height * settings.BORDER_TOP):int(height * settings.BORDER_BOTTOM), :, np.newaxis])
-        ans[:, index, :] = s[:, :, 0]
+        # in get_principal_slices get np.ndarray with shape (height, width, 1), i guess it's specter
+        s: np.ndarray = slices.get_principal_slices(frame[int(height * settings.BORDER_TOP):int(height * settings.BORDER_BOTTOM), :, np.newaxis])
+        
+        # in s return array with the same shape (height, width, 1)
+
+        cube[:, index, :] = s[:, :, 0]
         if cv2.waitKey(1) & 0xFF == ord(settings.EXIT_KEY):
             break
         index += 1
     capture.release()
 
-    if return_cube:
-        return ans
+    return cube
 
 def move_point(latitude, longitude, angle, length):
-    len_latitude = length * math.cos(angle)
-    len_longitude = length * math.sin(angle)
+    sin_a = math.sin(angle)
+    cos_a = math.cos(angle)
     return (
-        latitude + len_latitude,
-        longitude + len_longitude,
+        latitude + length * cos_a,
+        longitude + length * sin_a,
     )
 
-def interp(lines, latitude, longitude, rel_alt, angle):
+
+def interp(lines: np.ndarray, latitude: list, longitude: list, rel_alt: list, angle: list) -> np.ndarray:
+    """
+        This code implements interpolation of 3D uav data.
+        The input data is a 3D array of shape (n, m, k), where n is the number of spectral bands,
+        m is the number of points in the flight path, and k is the number of points along the line.
+        
+        The function takes 4 additional arguments:
+        latitude, longitude, rel_alt, angle - lists of length m, which contain the coordinates of the points
+        of the flight path, the relative altitude and the angle of the camera at each point.
+        The code transforms the coordinates of each line and maps the data to a 2D plane.
+        This is done by looping over each line and first adjusting the atlitude by diving rel_alt by the cosine of the camera pitch.
+        Then, the latitude and longitude are shifted using the adjusted altitude, angle. and camera pitch.
+    """
     n, m, k = lines.shape
     x = []
     y = []
     z = []
+    cos_pitch = math.cos(math.radians(settings.CAMERA_PITCH))
+    tan_pitch = math.tan(math.radians(settings.CAMERA_PITCH))
+    pi_2 = math.pi / 2.0
     for j in range(m):
-        rel_alt[j] /= math.cos(math.radians(settings.CAMERA_PITCH))
+        rel_alt[j] /= cos_pitch
         w = rel_alt[j] * settings.CAMERA_TANGENT
-        latitude[j] += rel_alt[j] * math.tan(math.radians(settings.CAMERA_PITCH)) * math.cos(angle[j])
-        longitude[j] += rel_alt[j] * math.tan(math.radians(settings.CAMERA_PITCH)) * math.sin(angle[j])
-        x_1, y_1 = move_point(latitude[j], longitude[j], angle[j] + math.pi / 2.0, w / 2.0)
-        x_2, y_2 = move_point(latitude[j], longitude[j], angle[j] - math.pi / 2.0, w / 2.0)
+        latitude[j] += rel_alt[j] * tan_pitch * math.cos(angle[j])
+        longitude[j] += rel_alt[j] * tan_pitch * math.sin(angle[j])
+        x_1, y_1 = move_point(latitude[j], longitude[j], angle[j] + pi_2, w / 2.0)
+        x_2, y_2 = move_point(latitude[j], longitude[j], angle[j] - pi_2, w / 2.0)
         x.extend(np.linspace(x_1, x_2, k))
         y.extend(np.linspace(y_1, y_2, k))
         for index in range(k):
@@ -103,12 +132,14 @@ def interp(lines, latitude, longitude, rel_alt, angle):
     return ans
 
 
-def build_hypercube_by_videos(dir_input, gps_filename):
+def build_hypercube_by_videos(dir_input: str, gps_filename: str) -> np.ndarray:
     cubes = []
-    for filename in load_data(dir_input, ".avi"):
-        print(filename)
-        cube = save_slices(filename, return_cube=True)
-        cubes.append(cube)
+    for filename in load_data(dir_input, ".avi"): #filename = dir_input + "video_1.avi"
+        cube: np.ndarray = save_slices(filename) 
+        print(cube.shape)
+        cubes.append(cube) # every cube have a shape (40, num of frames, width)
+    
     cube = np.concatenate(cubes, axis=1)
+
     cube = build_by_gps_log(cube, gps_filename)
     return cube
--- Gaidel_Legacy/slices.py
+++ Gaidel_Legacy/slices.py
@@ -1,24 +1,31 @@
 import math
-import numpy
+import numpy as np
 import Gaidel_Legacy.settings as settings
 
 def gaussian(length, mean, std):
-    return numpy.exp(-((numpy.arange(0, length) - mean) ** 2) / 2.0 / (std ** 2)) / math.sqrt(2.0 * math.pi) / std
+    return np.exp(-((np.arange(0, length) - mean) ** 2) / 2.0 / (std ** 2)) / math.sqrt(2.0 * math.pi) / std
 
-def get_principal_slices(spectrum):
-    n, m, k = spectrum.shape
+def get_principal_slices(spectrum: np.ndarray) -> np.ndarray:
+    n, m, k = spectrum.shape 
+    # n: height of frame, m: width of frame, k = 1
+
     width = n // settings.config.spectral_bands_number
-    gaussian_window = gaussian(width, width / 2.0, width / 6.0)
-    mid = len(gaussian_window) // 2
-    gaussian_window[mid] = 1.0 - numpy.sum(gaussian_window) + gaussian_window[mid]
-    ans = numpy.zeros(shape=(settings.config.spectral_bands_number, m, k), dtype=numpy.uint8)
+    gaussian_window = gaussian(width, width / 2.0, width / 6.0) 
+    # gaussian_window contain few float data
+    
+    mid = len(gaussian_window) // 2 
+    # center idx of gaussian_window list
+    
+    gaussian_window[mid] = 1.0 - gaussian_window[:mid].sum() - gaussian_window[mid+1:].sum()
+
+    ans = np.zeros((settings.config.spectral_bands_number, m, k), dtype=np.uint8)
+    # create empty array with shape (40, width of frame, 1)
+
     for j in range(settings.config.spectral_bands_number):
         left_bound = j * n // settings.config.spectral_bands_number
-        ans[j, :, :] = numpy.tensordot(
-            spectrum[left_bound:left_bound + len(gaussian_window), :, :],
-            gaussian_window,
-            axes=([0], [0]),
-        )
-    return ans, numpy.linspace(
-            settings.WAVELENGTH_MIN, settings.WAVELENGTH_MAX, settings.config.spectral_bands_number
-    ).tolist()
+
+        ans[j, :, :] = np.tensordot(spectrum[left_bound:left_bound + len(gaussian_window), :, :],
+                                    gaussian_window,
+                                    axes=([0], [0]),)
+
+    return ans
