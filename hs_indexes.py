4a4104f85fce35749a35af38afaa280520de4343
andremakar <andre_makar@bk.ru>
2023-02-13 16:30:37 +0400

    fix hs_indexes.py

--- hs_indexes.py
+++ hs_indexes.py
@@ -1,54 +1,60 @@
 import numpy as np
 
-# TODO which type is w_data?
-# TODO rewrite docstring
-def ndvi_mask(cube: np.ndarray, w_data: str):
+
+def get_band_numbers(left_border: int, right_border: int, w_data: list) -> tuple:
     """
-    ____________
-    necessary to read three-dimensional images and a text file with wavelengths
-    ____________
-    Parameters
-    ----------
-    cube : np.ndarray, 
-    w_dana : str
-            """
-    # TODO bad naming
-    def sps(x, y, w_data):
+    get_band_numbers(left_border, right_border, w_data)
 
-        # The input is supplied with wavelengths in the form of a list
-        wd = [i for i, v in enumerate(w_data) if x < v < y]
-        # TODO bad naming
-        sps = [min(wd), max(wd)] 
-        return sps
-
-    # TODO bad naming
-    def reduceMean(HSI, l_bound, r_bound):
-        return np.mean(HSI[:, :, l_bound:r_bound], axis=2)
-
-    # TODO replace code in ndvi_mask
-    def NDVI(cube):
-
-        # TODO bad naming
-        r1 = 633
-        # TODO bad naming
-        r2 = 650
-        # TODO bad naming
-        red_sps = sps(r1, r2, w_data)
-
-        # TODO bad naming
-        n1 = 844
-        # TODO bad naming
-        n2 = 860
-        # TODO bad naming
-        nir_sps = sps(n1, n2, w_data)
-
-        red = reduceMean(cube, red_sps[0], red_sps[1])
-        nir = reduceMean(cube, nir_sps[0], nir_sps[1])
-
-        # TODO normalize values in [0, 1]
-        # TODO add zero-replacement elements where nir + red = 0.0
-        # TODO delete + 0.0001
-        return (nir - red)/(nir + red+0.0001)
+        The input is supplied with wavelengths in the form of a list
+    """
+    part_bands_list = [i for i, v in enumerate(w_data) if left_border < v < right_border]
     
-    mask = NDVI(cube)
-    return mask
+    return min(part_bands_list), max(part_bands_list)
+
+   
+def reduce_mean(HSI: np.ndarray, l_bound: int, r_bound: int) -> int:
+    """
+    reduceMean(HSI, l_bound, r_bound):
+
+        Calculating average values from multiple channels
+    
+    """
+    return np.mean(HSI[:, :, l_bound:r_bound], axis=2)
+
+
+def ndvi_mask(cube: np.ndarray, 
+              w_data: list, 
+              left_red = 633, 
+              right_red = 650, 
+              left_nir = 844, 
+              right_nir = 860) -> np.ndarray: 
+    
+    """
+    ndvi_mask(cube: np.ndarray, w_data: list)
+    
+        Calculating the NDVI index
+        
+        Parameters
+        ----------
+        cube: np.ndarray
+           hyperspectral image
+
+        w_data: list
+            list of hyperspectral images wavelengths
+
+        Return 
+        ------
+            np.ndarray        
+        """
+    
+    red_band_numbers = get_band_numbers(left_red, right_red, w_data)
+    nir_band_numbers = get_band_numbers(left_nir, right_nir, w_data)
+
+    red = reduce_mean(cube, red_band_numbers[0], red_band_numbers[1])
+    nir = reduce_mean(cube, nir_band_numbers[0], nir_band_numbers[1])
+
+    mask = (nir - red)/(nir + red) + 1
+    mask[nir + red == 0] = 0 
+    mask_2 = np.copy(mask)
+    mask_2 = ((mask_2 - mask_2.min())/(mask_2.max() - mask_2.min()))
+    return mask_2
