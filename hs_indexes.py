d0c2ab167b17f2ce3bb1feefac31e68828bdd22d
f-neumann77 <f.neumann77@yandex.ru>
2023-02-14 10:50:35 +0400

    - added TODOs for andre_makar

--- hs_indexes.py
+++ hs_indexes.py
@@ -1,34 +1,36 @@
 import numpy as np
 
 
+# TODO in docstring not exists parameters description
 def get_band_numbers(left_border: int, right_border: int, w_data: list) -> tuple:
     """
     get_band_numbers(left_border, right_border, w_data)
-
+        # TODO in docstring bad description of function. It not make sense
         The input is supplied with wavelengths in the form of a list
     """
     part_bands_list = [i for i, v in enumerate(w_data) if left_border < v < right_border]
-    
+
     return min(part_bands_list), max(part_bands_list)
 
-   
-def reduce_mean(HSI: np.ndarray, l_bound: int, r_bound: int) -> int:
+
+# TODO in docstring not exists parameters description
+def reduce_mean(hsi: np.ndarray, l_bound: int, r_bound: int) -> np.ndarray:
     """
     reduceMean(HSI, l_bound, r_bound):
 
         Calculating average values from multiple channels
     
     """
-    return np.mean(HSI[:, :, l_bound:r_bound], axis=2)
+    return np.mean(hsi[:, :, l_bound:r_bound], axis=2)
 
 
-def ndvi_mask(cube: np.ndarray, 
-              w_data: list, 
-              left_red = 633, 
-              right_red = 650, 
-              left_nir = 844, 
-              right_nir = 860) -> np.ndarray: 
-    
+# TODO in docstring not exists all parameters
+def ndvi_mask(cube: np.ndarray,
+              w_data: list,
+              left_red = 633,
+              right_red = 650,
+              left_nir = 844,
+              right_nir = 860) -> np.ndarray:
     """
     ndvi_mask(cube: np.ndarray, w_data: list)
     
@@ -46,15 +48,16 @@ def ndvi_mask(cube: np.ndarray,
         ------
             np.ndarray        
         """
-    
+
     red_band_numbers = get_band_numbers(left_red, right_red, w_data)
     nir_band_numbers = get_band_numbers(left_nir, right_nir, w_data)
 
     red = reduce_mean(cube, red_band_numbers[0], red_band_numbers[1])
     nir = reduce_mean(cube, nir_band_numbers[0], nir_band_numbers[1])
 
-    mask = (nir - red)/(nir + red) + 1
-    mask[nir + red == 0] = 0 
+    mask = (nir - red) / (nir + red) + 1
+    mask[nir + red == 0] = 0
+    # TODO reason copying? Use only mask
     mask_2 = np.copy(mask)
-    mask_2 = ((mask_2 - mask_2.min())/(mask_2.max() - mask_2.min()))
+    mask_2 = ((mask_2 - mask_2.min) / (mask_2.max - mask_2.min))
     return mask_2
