8b0e76c638f4f523d20379cfe5a68ea3b2afbcfb
f-neumann77 <f.neumann77@yandex.ru>
2023-03-07 10:52:52 +0400

    hs_indexes.py:     -added TODO

--- hs_indexes.py
+++ hs_indexes.py
@@ -3,6 +3,7 @@ from typing import Union
 from hsi import HSImage
 from scipy.interpolate import interp1d
 
+
 def normalization(mask: np.ndarray) -> np.ndarray:
     """
     normalization(mask)
@@ -12,7 +13,7 @@ def normalization(mask: np.ndarray) -> np.ndarray:
         Parameters
         ----------
         mask: np.ndarray
-            unnormalized array
+            Unnormalized array
         Return 
         ------
             np.ndarray
@@ -21,8 +22,22 @@ def normalization(mask: np.ndarray) -> np.ndarray:
     return (mask - mask.min()) / (mask.max() - mask.min())
 
 
-def neighbor_el(l: list, el: float) -> float:
-    return min(l, key=lambda x:abs(x-el))
+def neighbor_el(elements_list: list, element: float) -> float:
+    """
+    neighbor_el(l, el)
+        Return the closest element from list to given element
+
+        Parameters
+        ----------
+        elements_list: list
+
+        element: float
+
+        Returns
+        -------
+            float
+    """
+    return min(elements_list, key=lambda x: abs(x - element))
 
 
 def get_band_numbers(w_l: int, w_data: Union[list, np.ndarray]) -> int:
@@ -30,7 +45,7 @@ def get_band_numbers(w_l: int, w_data: Union[list, np.ndarray]) -> int:
     get_band_numbers(w_l, w_data)
 
         Returns a tuple of two channel values with minimum and maximum wavelength values
-
+        # TODO update this docstring: not actual variables names
         Parameters
         ----------
         left_border: int
@@ -46,7 +61,7 @@ def get_band_numbers(w_l: int, w_data: Union[list, np.ndarray]) -> int:
         ------
             int     
     """
-    
+
     if w_l in w_data:
         w_data = list(w_data)
         return w_data.index(w_l)
@@ -59,7 +74,7 @@ def get_band_numbers(w_l: int, w_data: Union[list, np.ndarray]) -> int:
         return index_new_wl
 
 
-def ndvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=False) -> np.ndarray:
+def ndvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray] = False) -> np.ndarray:
     """
     ndvi_mask(cube, w_data)
         #TODO добавить ссылку на описание индекса
@@ -77,11 +92,11 @@ def ndvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=
         ------
             np.ndarray        
     """
-    
+
     if type(cube) == HSImage:
         cube_data = cube.data
         w_data = cube.wavelengths
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
@@ -101,7 +116,7 @@ def ndvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=
     return normalization(mask)
 
 
-def dvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=False) -> np.ndarray:
+def dvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray] = False) -> np.ndarray:
     """
     dvi_mask(cube, w_data)
         #TODO добавить ссылку на описание индекса
@@ -119,11 +134,11 @@ def dvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=F
         ------
             np.ndarray        
     """
-    
+
     if type(cube) == HSImage:
         cube_data = cube.data
         w_data = cube.wavelengths
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
@@ -131,7 +146,7 @@ def dvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=F
     wl_700 = 700
     wl_800 = 800
 
-    band_numbers_700 = get_band_numbers(wl_700,  w_data)
+    band_numbers_700 = get_band_numbers(wl_700, w_data)
     band_numbers_800 = get_band_numbers(wl_800, w_data)
 
     channel_700 = cube_data[:, :, band_numbers_700]
@@ -142,7 +157,7 @@ def dvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=F
     return normalization(mask)
 
 
-def osavi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=False) -> np.ndarray:
+def osavi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray] = False) -> np.ndarray:
     """
     osavi_mask(cube, w_data)
         #TODO добавить ссылку на описание индекса
@@ -164,7 +179,7 @@ def osavi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]
     if type(cube) == HSImage:
         cube_data = cube.data
         w_data = cube.wavelengths
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
@@ -184,7 +199,7 @@ def osavi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]
     return normalization(mask)
 
 
-def sr_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=False) -> np.ndarray:
+def sr_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray] = False) -> np.ndarray:
     """
     sr_mask(cube, w_data)
         #TODO добавить ссылку на описание индекса
@@ -206,7 +221,7 @@ def sr_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=Fa
     if type(cube) == HSImage:
         cube_data = cube.data
         w_data = cube.wavelengths
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
@@ -226,7 +241,7 @@ def sr_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=Fa
     return normalization(mask)
 
 
-def wdrvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=False) -> np.ndarray:
+def wdrvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray] = False) -> np.ndarray:
     """
     wdrvi_mask(cube, w_data)
         #TODO добавить ссылку на описание индекса
@@ -248,7 +263,7 @@ def wdrvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]
     if type(cube) == HSImage:
         cube_data = cube.data
         w_data = cube.wavelengths
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
@@ -262,13 +277,13 @@ def wdrvi_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]
     channel_680 = cube_data[:, :, band_numbers_680]
     channel_800 = cube_data[:, :, band_numbers_800]
 
-    mask = (0.05 * channel_800 - channel_680)/(0.05 * channel_800 + channel_680)
+    mask = (0.05 * channel_800 - channel_680) / (0.05 * channel_800 + channel_680)
     mask[0.05 * channel_800 + channel_680 == 0] = 0
 
     return normalization(mask)
 
 
-def mtvi2_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=False) -> np.ndarray:
+def mtvi2_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray] = False) -> np.ndarray:
     """
     mtvi2_mask(cube, w_data)
         #TODO добавить ссылку на описание индекса
@@ -290,7 +305,7 @@ def mtvi2_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]
     if type(cube) == HSImage:
         cube_data = cube.data
         w_data = cube.wavelengths
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
@@ -308,14 +323,14 @@ def mtvi2_mask(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]
     channel_800 = cube_data[:, :, band_numbers_800]
 
     a = 1.5 * (1.2 * (channel_800 - channel_550) - 2.5 * (channel_670 - channel_550))
-    b = np.sqrt((2 * channel_800 + 1)**2 - (6 * channel_800 - 5 * np.sqrt(channel_670)) - 0.5)
+    b = np.sqrt((2 * channel_800 + 1) ** 2 - (6 * channel_800 - 5 * np.sqrt(channel_670)) - 0.5)
     mask = a * b
     mask[np.isnan(mask) == True] = 0
 
     return normalization(mask)
 
 
-def simple_hsi_to_rgb(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray]=False) -> np.ndarray:
+def simple_hsi_to_rgb(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.ndarray] = False) -> np.ndarray:
     """
     hsi_for_rgb(cube, w_data)
 
@@ -333,15 +348,15 @@ def simple_hsi_to_rgb(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.n
         ------
             np.ndarray        
     """
-    
+
     if type(cube) == HSImage:
         cube_data = cube.data
         w_data = cube.wavelengths
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
-        
+
     wl_440 = 440
     wl_550 = 550
     wl_640 = 640
@@ -358,16 +373,16 @@ def simple_hsi_to_rgb(cube: Union[HSImage, np.ndarray], w_data: Union[list, np.n
 
 
 def XYZ2sRGB_exgamma(XYZ):
-
-    # See IEC_61966-2-1.pdf
-    # No gamma correction has been incorporated here, nor any clipping, so this
-    # transformation remains strictly linear.  Nor is there any data-checking.
-    # DHF 9-Feb-11
-
+    """
+    See IEC_61966-2-1.pdf
+    No gamma correction has been incorporated here, nor any clipping, so this
+    transformation remains strictly linear.  Nor is there any data-checking.
+    DHF 9-Feb-11
+    """
     # Image dimensions
     d = XYZ.shape
-    r = d[0] * d[1]   
-    w = d[2]             
+    r = d[0] * d[1]
+    w = d[2]
 
     # Reshape for calculation, converting to w columns with r rows.
     XYZ = np.reshape(XYZ, (r, w))
@@ -387,7 +402,8 @@ def XYZ2sRGB_exgamma(XYZ):
     return sRGB
 
 
-def hsi_to_rgb(cube: Union[HSImage, np.ndarray], illum_coef: np.ndarray, xyzbar: np.ndarray) -> np.ndarray: 
+# TODO finalize it.
+def hsi_to_rgb(cube: Union[HSImage, np.ndarray], illum_coef: np.ndarray, xyzbar: np.ndarray) -> np.ndarray:
     """
     hsi_to_rgb(cube, illum_coef, xyzbar)
 
@@ -409,27 +425,26 @@ def hsi_to_rgb(cube: Union[HSImage, np.ndarray], illum_coef: np.ndarray, xyzbar:
 
 
     """
-    
+
     if type(cube) == HSImage:
         cube_data = cube.data
-    
+
     elif type(cube) == np.ndarray:
         cube_data = cube
         del cube
 
     wavelengths = list(np.linspace(400, 720, 33))
-    
-    wavelengths_cube = list(np.linspace(420, 980, cube_data.shape[-1]))
-    right_bound = wavelengths_cube.index(neighbor_el(wavelengths_cube, 720)) 
-    rgb_waves = wavelengths_cube[:right_bound]
-   
 
-    #Функция которая в видимом диапазоне 
-    f_illum = interp1d(wavelengths, illum_coef[:,0])
+    wavelengths_cube = list(np.linspace(420, 980, cube_data.shape[-1]))
+    right_bound = wavelengths_cube.index(neighbor_el(wavelengths_cube, 720))
+    rgb_waves = wavelengths_cube[:right_bound]
+
+    # Функция которая в видимом диапазоне
+    f_illum = interp1d(wavelengths, illum_coef[:, 0])
 
     new_cube = np.zeros((np.shape(cube_data)[0], np.shape(cube_data)[1], right_bound))
-    for i, w in zip(range(new_cube.shape[-1]), rgb_waves): 
-        new_cube[:,:,i] = cube_data[:,:,i] * f_illum(w)
+    for i, w in zip(range(new_cube.shape[-1]), rgb_waves):
+        new_cube[:, :, i] = cube_data[:, :, i] * f_illum(w)
 
     r, c, w = new_cube.shape
     radiances = np.reshape(new_cube, (r * c, w))
@@ -448,19 +463,18 @@ def hsi_to_rgb(cube: Union[HSImage, np.ndarray], illum_coef: np.ndarray, xyzbar:
     xyz_2 = [f_2(i) for i in rgb_waves]
 
     xyzbar_new = (np.array([xyz_0, xyz_1, xyz_2])).T
-    
+
     XYZ = np.dot(radiances, xyzbar_new)
     XYZ = np.reshape(XYZ, (r, c, 3))
     XYZ = (XYZ - np.min(XYZ)) / (np.max(XYZ) - np.min(XYZ))
-    
+
     RGB = XYZ2sRGB_exgamma(XYZ)
     RGB = (RGB - np.min(RGB)) / (np.max(RGB) - np.min(RGB))
 
-   
     gray_mean = np.mean(RGB, axis=2)
     un = np.unique(gray_mean)
 
-    #Так оставить?
+    # Так оставить?
     thresh = 0.98
     coord = [np.where(gray_mean == un[int(len(un) * thresh - 1)])][0]
     x, y = coord
