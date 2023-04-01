1da89ad1bff67de82a3894ba5e8f13996593ebe0
Max Petrov <underburned@gmail.com>
2023-04-02 01:03:39 +0400

    add: utils: dict save and load, todos

--- openhsl/utils.py
+++ openhsl/utils.py
@@ -1,9 +1,12 @@
 import datetime
+import json
 import math
 import numpy as np
 from pathlib import Path
+from typing import Optional
 
 
+# TODO rename to path_exists
 def dir_exists(path: str) -> bool:
     return Path(path).exists()
 
@@ -24,6 +27,19 @@ def load_data(path: str, exts: list) -> list:
     return [str(p) for p in Path(path).glob("*") if p.suffix[1:] in exts]
 
 
+def load_dict_from_json(path: str) -> Optional[dict]:
+    data: Optional[dict] = None
+    with open(path, 'r') as handle:
+        data = json.load(handle)
+    return data
+
+
+def save_dict_to_json(data: dict, path: str) -> None:
+    with open(path, 'w') as handle:
+        json.dump(data, handle, indent=4)
+
+
+# TODO move into hs_image_utils
 def gaussian(length: int, mean: float, std: float) -> np.ndarray:
     return np.exp(-((np.arange(0, length) - mean) ** 2) / 2.0 / (std ** 2)) / math.sqrt(2.0 * math.pi) / std
 
