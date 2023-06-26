3d2ed591c3904e81272d39459f24fee39cc39c70
by-LZ-for <82704394+by-LZ-for@users.noreply.github.com>
2023-06-26 14:16:06 +0400

    Delete gui_marker directory

--- gui_marker/src/model/Layer.py
+++
@@ -1,8 +0,0 @@
-class Layer:
-    def __init__(self, json_layer, cv_mask):
-        self.name = json_layer["name"]
-        self.color = json_layer["color"]
-        self.isVisible = json_layer["visible"]
-        self.isLocked = json_layer["locked"]
-
-        self.cvMask = cv_mask
