89c7822caaf50a3231a656ff8ac0ba7aa657a518
f-neumann77 <f.neumann77@yandex.ru>
2024-03-29 12:17:52 +0400

    - update tests for time-scoring of models inference

--- tests/test_models_inference_time.py
+++ tests/test_models_inference_time.py
@@ -10,14 +10,14 @@
 
 """
 from typing import Tuple
-
+import numpy as np
 import pytest
 from sklearn.metrics import accuracy_score
 from time import time
 
 from openhsl.hsi import HSImage
 from openhsl.hs_mask import HSMask
-from openhsl.data.utils import apply_pca
+from openhsl.data.utils import apply_pca, HyperStandardScaler
 
 from openhsl.models.fcnn import FCNN
 from openhsl.models.hsicnn_luo import HSICNN as Luo
@@ -33,18 +33,11 @@ from openhsl.models.ssftt import SSFTT
 
 @pytest.fixture
 def return_inference_test_data():
-    hsi = HSImage()
-    mask = HSMask()
-    hsi.load(path_to_data='../test_data/nn_tests/three_coffee_piles_small2.mat', key='image')
-    mask.load(path_to_data='../test_data/nn_tests/three_coffee_piles_small2_gt.mat', key='img')
+    hsi = HSImage(np.random.rand(200, 200, 250))
+    mask = HSMask(np.random.randint(5, size=(200, 200)))
+    scaler = HyperStandardScaler()
+    hsi.data = scaler.fit_transform(hsi.data)
     return hsi, mask
-
-
-def get_classification_accuracy(pretrained_model,
-                                dataset: Tuple):
-    X, y = dataset
-    predictions = pretrained_model.predict(X)
-    return accuracy_score(predictions.flatten(), y.flatten()) > 0.8
 
 
 def get_inference_time(pretrained_model,
@@ -60,55 +53,10 @@ def get_inference_time(pretrained_model,
     return final_time - start_time < 40
 
 
-def get_accuracy(pretrained_model,
-                 dataset: Tuple):
-    pass
-
-
-def test_baseline(return_inference_test_data):
-    n_classes = len(return_inference_test_data[1])
-
-    model = FCNN(n_classes=n_classes, device='cuda', n_bands=250)
-
-    assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data)
-
-
-def test_hsicnn_luo(return_inference_test_data):
-    n_classes = len(return_inference_test_data[1])
-
-    model = Luo(n_classes=n_classes, device='cuda', n_bands=250)
-
-    assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data)
-
-
 def test_m1dcnn(return_inference_test_data):
     n_classes = len(return_inference_test_data[1])
 
     model = M1DCNN(n_classes=n_classes, device='cuda', n_bands=250)
-
-    assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data)
-
-
-def test_m3dcnn_sharma(return_inference_test_data):
-    n_classes = len(return_inference_test_data[1])
-
-    model = SHARMA(n_classes=n_classes, device='cuda', n_bands=30)
-
-    assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data, num_components=30)
-
-
-def test_m3dcnn_hamida(return_inference_test_data):
-    n_classes = len(return_inference_test_data[1])
-
-    model = HAMIDA(n_classes=n_classes, device='cuda', n_bands=250)
-
-    assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data)
-
-
-def test_m3dcnn_he(return_inference_test_data):
-    n_classes = len(return_inference_test_data[1])
-
-    model = HE(n_classes=n_classes, device='cuda', n_bands=250)
 
     assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data)
 
@@ -140,6 +88,6 @@ def test_tf2dcnn(return_inference_test_data):
 def test_ssftt(return_inference_test_data):
     n_classes = len(return_inference_test_data[1])
 
-    model = SSFTT(n_classes=n_classes, device='cuda', n_bands=30)
+    model = SSFTT(n_classes=n_classes, device='cuda', n_bands=250)
 
-    assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data, num_components=30)
+    assert get_inference_time(pretrained_model=model, dataset=return_inference_test_data)
