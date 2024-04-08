0943f72cf41ce0c7abf5f8bb06011e5f68e8d80b
Max Petrov <underburned@gmail.com>
2024-04-08 16:46:58 +0400

    fix: utils after merge

--- openhsl/utils.py
+++ openhsl/utils.py
@@ -1,3 +1,5 @@
+import datetime
+import json
 import math
 import matplotlib.patches as mpatches
 import numpy as np
@@ -14,7 +16,7 @@ from scipy.stats import ttest_ind
 from matplotlib import pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import trange
-from typing import Union, List, Tuple, Literal
+from typing import List, Literal, Optional, Tuple, Union
 
 from openhsl.data.utils import convert_to_color_, get_palette
 from openhsl.hsi import HSImage
@@ -631,3 +633,46 @@ def hsi_to_rgb(hsi: HSImage,
     rgb = contrast_correction(rgb, gamma_thresh)
 
     return rgb
+
+
+# TODO rename to path_exists
+def dir_exists(path: str) -> bool:
+    return Path(path).exists()
+
+
+def get_current_date(date_format_str="%d.%m.%Y") -> str:
+    return datetime.datetime.now().strftime(date_format_str)
+
+
+def get_current_time(time_format_str="%H:%M:%S") -> str:
+    return datetime.datetime.now().strftime(time_format_str)
+
+
+def key_exists_in_dict(dict_var: dict, key: str) -> bool:
+    return key in dict_var
+
+
+def get_file_complete_name(path: str) -> str:
+    p = Path(path)
+    return p.name
+
+
+def load_data(path: str, exts: list) -> list:
+    return [str(p) for p in Path(path).glob("*") if p.suffix[1:] in exts]
+
+
+def load_dict_from_json(path: str) -> Optional[dict]:
+    data: Optional[dict] = None
+    with open(path, 'r') as handle:
+        data = json.load(handle)
+    return data
+
+
+def save_dict_to_json(data: dict, path: str) -> None:
+    with open(path, 'w') as handle:
+        json.dump(data, handle, indent=4)
+
+
+# TODO move into hs_image_utils
+def gaussian(length: int, mean: float, std: float) -> np.ndarray:
+    return np.exp(-((np.arange(0, length) - mean) ** 2) / 2.0 / (std ** 2)) / math.sqrt(2.0 * math.pi) / std
