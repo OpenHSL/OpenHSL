50f9d0c96894174447da8c13bd952d67156e4055
by-LZ-for <82704394+by-LZ-for@users.noreply.github.com>
2024-05-24 11:17:14 +0400

    ai mask
    ai mask

--- annotator/Build_Win64_executable.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-pyinstaller --add-data "res/A.ico;res/" datmant.py
-
-echo Done.
--- annotator/GenerateUI_Win64.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-REM Run me in Anaconda Prompt with the corresponding environment enabled!
-
-echo Running pyuic5...
-call pyuic5 ui\datmant.ui -o ui\datmant_ui.py
-call pyuic5 ui\color_specs.ui -o ui\color_specs_ui.py
-echo Done.
